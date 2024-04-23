4688977ea1955caeabe1b040de667a1d21cf4690
Sebastien Gaillard <gaillard.sebastien@outlook.fr>
2024-04-23 17:12:12 +0200

    a

--- src/router/index.ts
+++ src/router/index.ts
@@ -55,7 +55,7 @@ const router = createRouter({
   routes,
 });
 
-router.beforeEach((to, from, next) => {
+router.beforeEach((to, _from, next) => {
   if (
     to.matched.some((record) => record.meta.requiresAuth) &&
     !store.state.isAuthenticated
